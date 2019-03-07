ec18b2acb7f332eb61ab77f8c4798c49de1e7878
xpoll <blmdz521@126.com>
2019-03-08 01:41:29 +0800

    'init'

--- src/main/java/cn/blmdz/admin/controller/GreetingController.java
+++ src/main/java/cn/blmdz/admin/controller/GreetingController.java
@@ -1,9 +1,13 @@
-package cn.blmdz.admin;
+package cn.blmdz.admin.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import cn.blmdz.admin.models.model.Greeting;
 
 @RestController
 public class GreetingController {
@@ -12,6 +16,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
+    @RequiresPermissions(value = { "" })
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
